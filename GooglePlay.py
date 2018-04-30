3fd262d55f61ff1a3209f8e78a87f712ac33ba96
Li-Jen Tang <b00701160@gmail.com>
2018-04-30 23:21:24 +0800

    new .py

--- GooglePlay.py
+++ GooglePlay.py
@@ -9,7 +9,6 @@ path = '/Users/charlie/Desktop/GooglePlay/'
 # Install latest Chrome driver:
 # https://sites.google.com/a/chromium.org/chromedriver/downloads
 driver = webdriver.Chrome(path+'chromedriver')
-
 # Page url
 driver.get('https://play.google.com/store/apps/collection/cluster?clp=wgYpCiUKH2NvbS5iaXRzbWVkaWEuYW5kcm9pZC5tdXNsaW1wcm8QARgDGAE%3D%3AS%3AANO1ljL05bg')
 
@@ -31,20 +30,16 @@ app = []
 org = []
 desc = []
 star = []
-for element in soup.find_all('a', {"class": "title"}):
-    url.append(domain+element.get('href'))
-    app.append(element.get('title'))
 
-for element in soup.find_all('a', {"class": "subtitle"}):
-    org.append(element.get('title'))
-for element in soup.find_all('div', {"class": "description"}):
-    desc.append(element.text)
-for element in soup.find_all('div', {"class": "tiny-star star-rating-non-editable-container"}):
-    star.append(element.get('aria-label').split()[1])
+for element in soup.find_all('div', {'class':'card-content id-track-click id-track-impression'}):
+	url.append(domain+element.find('a', {"class": "title"}).get('href'))
+	app.append(element.find('a', {"class": "title"}).get('title'))
+	org.append(element.find('a', {"class": "subtitle"}).get('title'))
+	desc.append(element.find('div', {"class": "description"}).text)
+	star.append(element.find('div', {"class": "tiny-star star-rating-non-editable-container"}).get('aria-label').split()[1])
 
 df = pd.DataFrame({'url':url, 'app':app, 'org':org, 'desc':desc, 'star':star})
 
 print len(df)
-print df.head()
 df.to_csv(path+'MuslimApp.csv', index=False, encoding='utf-8')
 driver.close()
